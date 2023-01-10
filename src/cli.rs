d1c71eccfb8136e671e9c11cda57c2bb387d6706
voyager-1 <cyhzero@gmail.com>
2023-01-10 23:55:35 +0800

    add cli

--- src/cli.rs
+++ src/cli.rs
@@ -12,6 +12,24 @@ pub(crate) struct Args {
 
 // 命令要可扩展
 #[derive(Subcommand, Debug, Clone)]
-enum Action {
+pub(crate) enum Action {
+    /// Diff two API response based on given profile
+    Run(RunArgs),
 
+}
+
+pub(crate) struct RunArgs {
+    /// profile name
+    #[clap(short, long, value_parser)]      // 如果不给定value_parser的话，就会用default parser把参数原原本本地解析为String
+    pub profile: String,
+
+    /// overrides args.Could be used to override the query, headers and body of the request.
+    #[clap(short, long, value_parser = parse_key_val, number_of_value = 1)] // number_of_value = 1让每个k-v参数都要在前面加上 "-x"
+    pub extra_params: Vec<KeyVal>
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct KeyVal {
+    key: String,
+    value: String,
 }
